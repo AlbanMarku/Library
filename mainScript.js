f6895640359510db2da9f15d271e9b2d9a580271
AlbanMarku <albanmarku@outlook.com>
2022-07-25 20:01:28 +0100

    Basic page layout.

--- mainScript.js
+++ mainScript.js
@@ -1,1 +1,31 @@
-alert("Hellow World!")
+let myLibrary = [];
+
+function Book(title, author, genre) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+}
+function FantasyBook(title, author, genre, typeOfFantasy) {
+    Book.call(this, title, author, genre);
+    this.typeOfFantasy = typeOfFantasy;
+}
+
+FantasyBook.prototype = Object.create(Book.prototype);
+
+Book.prototype.printInfo = function() {
+    return this.title
+}
+
+FantasyBook.prototype.printInfo = function() {
+    return `you got ${this.title} and you got ${this.author}`
+}
+
+FantasyBook.prototype.fantLore = function() {
+    return "The lore for this fant is that morder is bad"
+}
+
+// const myFantasyBook = new FantasyBook("lotr", "greg", "fantasy", "high Fantasy");
+// console.log(myFantasyBook.printInfo());
+// const book = new Book("got", "steve", "crime");
+// console.log(book.printInfo());
+// console.log(myFantasyBook.fantLore());
