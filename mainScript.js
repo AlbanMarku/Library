ebcd857f076588fcf91775a38cf28af331f8d37a
AlbanMarku <albanmarku@outlook.com>
2022-07-26 15:33:46 +0100

    New book modal.

--- mainScript.js
+++ mainScript.js
@@ -1,16 +1,22 @@
 const displayArea = document.querySelector("main");
-const card = document.createElement("div");
-const bookName = document.createElement("h2");
-const bookAuthor = document.createElement("h3");
-const pageArea = document.createElement("div");
-const sub1Btn = document.createElement("button");
-const sub5Btn = document.createElement("button");
-const add5Btn = document.createElement("button");
-const add1Btn = document.createElement("button");
-const numberDisplay = document.createElement("p");
-const buttonArea = document.createElement("div");
-const readButton = document.createElement("button");
-const removeButton = document.createElement("button");
+const addButton = document.querySelector("#addBookBtn");
+const openModalBtn = document.querySelector("[data-modal-target]");
+const closeModalBtn = document.querySelector("[data-close-Btn]");
+const overlay = document.getElementById("overlay");
+const modal = document.getElementById("modal");
+
+// const card = document.createElement("div");
+// const bookName = document.createElement("h2");
+// const bookAuthor = document.createElement("h3");
+// const pageArea = document.createElement("div");
+// const sub1Btn = document.createElement("button");
+// const sub5Btn = document.createElement("button");
+// const add5Btn = document.createElement("button");
+// const add1Btn = document.createElement("button");
+// const numberDisplay = document.createElement("p");
+// const buttonArea = document.createElement("div");
+// const readButton = document.createElement("button");
+// const removeButton = document.createElement("button");
 
 // card.classList.add("bookCard");
 // displayArea.appendChild(card);
@@ -44,20 +50,54 @@ function FantasyBook(title, author, genre, typeOfFantasy) {
 
 FantasyBook.prototype = Object.create(Book.prototype);
 
-Book.prototype.printInfo = function() {
-    return this.title
-}
+// Book.prototype.printInfo = function() {
+//     return this.title
+// }
 
-FantasyBook.prototype.printInfo = function() {
-    return `you got ${this.title} and you got ${this.author}`
-}
+// FantasyBook.prototype.printInfo = function() {
+//     return `you got ${this.title} and you got ${this.author}`
+// }
 
-FantasyBook.prototype.fantLore = function() {
-    return "The lore for this fant is that morder is bad"
-}
+// FantasyBook.prototype.fantLore = function() {
+//     return "The lore for this fant is that morder is bad"
+// }
 
 // const myFantasyBook = new FantasyBook("lotr", "greg", "fantasy", "high Fantasy");
 // console.log(myFantasyBook.printInfo());
 // const book = new Book("got", "steve", "crime");
 // console.log(book.printInfo());
 // console.log(myFantasyBook.fantLore());
+
+addButton.addEventListener("click", bookPrompt);
+
+function bookPrompt() {
+    
+}
+
+
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add("active");
+    overlay.classList.add("active");
+
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+
+}
+
+openModalBtn.addEventListener("click", () => {
+    openModal(modal);
+});
+
+closeModalBtn.addEventListener("click", () => {
+    closeModal(modal);
+});
+
+overlay.addEventListener("click", () => {
+    closeModal(modal);
+});
