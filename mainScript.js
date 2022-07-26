36ae60b0cb0074a356d56980e3da36c16d32a7d0
AlbanMarku <albanmarku@outlook.com>
2022-07-26 16:59:18 +0100

    Cards display library on screen.

--- mainScript.js
+++ mainScript.js
@@ -1,80 +1,103 @@
 const displayArea = document.querySelector("main");
-const addButton = document.querySelector("#addBookBtn");
 const openModalBtn = document.querySelector("[data-modal-target]");
 const closeModalBtn = document.querySelector("[data-close-Btn]");
 const overlay = document.getElementById("overlay");
 const modal = document.getElementById("modal");
-
-// const card = document.createElement("div");
-// const bookName = document.createElement("h2");
-// const bookAuthor = document.createElement("h3");
-// const pageArea = document.createElement("div");
-// const sub1Btn = document.createElement("button");
-// const sub5Btn = document.createElement("button");
-// const add5Btn = document.createElement("button");
-// const add1Btn = document.createElement("button");
-// const numberDisplay = document.createElement("p");
-// const buttonArea = document.createElement("div");
-// const readButton = document.createElement("button");
-// const removeButton = document.createElement("button");
-
-// card.classList.add("bookCard");
-// displayArea.appendChild(card);
-// bookName.textContent = "lotr";
-// card.appendChild(bookName);
-// bookAuthor.textContent = "jrr";
-// card.appendChild(bookAuthor);
-
-// pageArea.classList.add("pageArea");
-// card.appendChild(pageArea);
-// sub1Btn.textContent = "-1";
-// sub1Btn.classList.add("incrementBtn");
-// pageArea.appendChild(sub1Btn);
-// sub5Btn.textContent = "-5";
-// sub5Btn.classList.add("incrementBtn");
-// pageArea.appendChild(sub5Btn);
-
-//and so on...
+const form = document.querySelector("form");
 
 let myLibrary = [];
 
-function Book(title, author, genre) {
+function Book(title, author, pages, pagesRead, isRead) {
     this.title = title;
     this.author = author;
-    this.genre = genre;
-}
-function FantasyBook(title, author, genre, typeOfFantasy) {
-    Book.call(this, title, author, genre);
-    this.typeOfFantasy = typeOfFantasy;
-}
+    this.pages = pages;
+    this.isRead = isRead;
 
-FantasyBook.prototype = Object.create(Book.prototype);
-
-// Book.prototype.printInfo = function() {
-//     return this.title
-// }
-
-// FantasyBook.prototype.printInfo = function() {
-//     return `you got ${this.title} and you got ${this.author}`
-// }
-
-// FantasyBook.prototype.fantLore = function() {
-//     return "The lore for this fant is that morder is bad"
-// }
-
-// const myFantasyBook = new FantasyBook("lotr", "greg", "fantasy", "high Fantasy");
-// console.log(myFantasyBook.printInfo());
-// const book = new Book("got", "steve", "crime");
-// console.log(book.printInfo());
-// console.log(myFantasyBook.fantLore());
-
-addButton.addEventListener("click", bookPrompt);
-
-function bookPrompt() {
-    
+    if (pagesRead === null || pagesRead === "") {
+        this.pagesRead = 0;
+    } else {
+        this.pagesRead = pagesRead;
+    }
 }
 
+function getBook() {
+    const titleInput = document.getElementById("title").value;
+    const authorInput = document.getElementById("author").value;
+    const pagesInput = document.getElementById("pages").value;
+    const pagesReadInput = document.getElementById("pagesRead").value;
+    const isReadInput = document.getElementById("isRead").checked;
 
+    return new Book(titleInput,authorInput,pagesInput,pagesReadInput,isReadInput)
+}
+
+function addToLibrary(newBook) {
+    myLibrary.push(newBook);
+    clearGrid();
+    addLibraryToGrid();
+}
+
+function addLibraryToGrid() {
+    for (let selectedBook of myLibrary) {
+        createCardForBook(selectedBook);
+    }
+}
+
+function createCardForBook(book) {
+    const card = document.createElement("div");
+    const bookName = document.createElement("h2");
+    const bookAuthor = document.createElement("h3");
+    const pageArea = document.createElement("div");
+    const sub1Btn = document.createElement("button");
+    const sub5Btn = document.createElement("button");
+    const add5Btn = document.createElement("button");
+    const add1Btn = document.createElement("button");
+    const numberArea = document.createElement("div");
+    const numberDisplay = document.createElement("p");
+    const buttonArea = document.createElement("div");
+    const readButton = document.createElement("button");
+    const removeButton = document.createElement("button");
+
+    card.classList.add("bookCard");
+    displayArea.appendChild(card);
+    bookName.textContent = `${book.title}`;
+    card.appendChild(bookName);
+    bookAuthor.textContent = `${book.author}`;
+    card.appendChild(bookAuthor);
+
+    pageArea.classList.add("pageArea");
+    card.appendChild(pageArea);
+    sub1Btn.textContent = "-1";
+    sub1Btn.classList.add("incrementBtn");
+    pageArea.appendChild(sub1Btn);
+    sub5Btn.textContent = "-5";
+    sub5Btn.classList.add("incrementBtn");
+    pageArea.appendChild(sub5Btn);
+    add5Btn.textContent = "+5";
+    add5Btn.classList.add("incrementBtn");
+    pageArea.appendChild(add5Btn);
+    add1Btn.textContent = "+1";
+    add1Btn.classList.add("incrementBtn");
+    pageArea.appendChild(add1Btn);
+
+    numberArea.classList.add("numberArea");
+    card.appendChild(numberArea);
+    numberDisplay.textContent = `${book.pagesRead}/${book.pages}`;
+    numberArea.appendChild(numberDisplay);
+
+    buttonArea.classList.add("buttonArea");
+    card.appendChild(buttonArea);
+    readButton.textContent = "Read";
+    readButton.classList.add("readBtn");
+    buttonArea.appendChild(readButton);
+    removeButton.textContent = "Remove";
+    removeButton.classList.add("removeBtn");
+    buttonArea.appendChild(removeButton);
+
+}
+
+function clearGrid() {
+    displayArea.innerHTML = "";
+}
 
 function openModal(modal) {
     if (modal == null) return
@@ -100,4 +123,11 @@ closeModalBtn.addEventListener("click", () => {
 
 overlay.addEventListener("click", () => {
     closeModal(modal);
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addToLibrary(getBook());
+    closeModal(modal);
+    form.reset();
 });
